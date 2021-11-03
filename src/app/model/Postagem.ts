e3b6b880df41dbc2b51cd42bc06704a5008fb170
Julio Teixeira <julioteixeirapimont@gmail.com>
2021-11-03 13:22:23 -0300

    deploy front end blog

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,11 @@
+import { Tema } from './Tema';
+import { User } from './User';
+
+export class Postagem{
+    public idPostagem: number
+    public titulo: string
+    public descricao: string
+    public fotoPostagem: string
+    public criador: User
+    public temaRelacionado: Tema
+}
